d9df1920c50a3f1d05684b8be6b0bd646aa31be3
RzkyFdly <57029311+rizky-fadly@users.noreply.github.com>
2026-01-15 10:36:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,21 @@ import joblib
 st.set_page_config(page_title="Credit Card Default Prediction")
 
 st.title("Credit Card Default Prediction")
-st.write("Prediksi risiko gagal bayar kartu kredit")
 
-# load model
 model = joblib.load("xgb_credit_default_model.pkl")
+feature_names = joblib.load("feature_names.pkl")
 
 st.subheader("Input Data Nasabah")
 
-input_data = {
-    "LIMIT_BAL": st.number_input("Limit Kredit", value=50000),
-    "AGE": st.number_input("Usia", value=30),
-    "PAY_0": st.number_input("Status Telat Bulan Terakhir", value=0),
-    "BILL_AMT1": st.number_input("Tagihan Bulan Terakhir", value=0),
-    "PAY_AMT1": st.number_input("Pembayaran Bulan Terakhir", value=0),
-}
+input_data = {}
+
+for col in feature_names:
+    input_data[col] = st.number_input(col, value=0.0)
 
 if st.button("Prediksi"):
     df_input = pd.DataFrame([input_data])
+    df_input = df_input[feature_names]  # urutan sama persis
+
     prob = model.predict_proba(df_input)[0][1]
 
     st.write(f"Probabilitas Default: **{prob:.2f}**")
